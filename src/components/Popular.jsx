587f9eeb5406d90c239711d547fb09ff40d7e4dc
Jared Gutkin <jgutkinsam@gmail.com>
2022-07-14 12:07:34 -0500

    css changed to out of file

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -3,6 +3,50 @@ import styled from "styled-components"
 import { Splide, SplideSlide } from '@splidejs/react-splide'
 import '@splidejs/react-splide/css'
 
+// const Wrapper = styled.div`
+//     margin: 4rem 0rem;
+// `;
+
+// const Card = styled.div
+// `
+//     min-height: 25rem;
+//     border-radius: 2rem;
+//     overflow: hidden;
+
+//     img {
+//         border-radius: 2rem;
+//         position: absolute;
+//         left: 0;
+//         width: 100%;
+//         height: 100%;
+//         object-fit: cover;
+//     }
+//     p {
+//         position: absolute;
+//         z-index: 10;
+//         left: 50%;
+//         bottom: 0%;
+//         transform: translate(-50%, 0%);
+//         color: white;
+//         width: 100%;
+//         text-align: center;
+//         font-weight: 600;
+//         font-size: 1rem;
+//         height: 40%;
+//         display: flex;
+//         justify-content: center;
+//         align-items: center;
+//     }
+// `;
+
+// const Gradient = styled.div`
+//     z-index: 3;
+//     position: absolute;
+//     width: 100%;
+//     height: 100%;
+//     background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5))
+// `;
+
 
 function Popular() {
     const [popular, setPopular] = useState([])
@@ -27,8 +71,8 @@ function Popular() {
     }
 
   return (
-    <>
-            <Wrapper>
+    <div>
+            <div className="wrapper">
                 <h3>Popular Picks</h3>
 
                 <Splide options={{
@@ -41,62 +85,20 @@ function Popular() {
                 {popular.map((recipe) => {
                     return(
                         <SplideSlide key={recipe.id}>
-                        <Card>
+                        <div className="card">
                             <p>{recipe.title}</p>
                             <img src={recipe.image} alt={recipe.title} />
-                            <Gradient/>
-                        </Card>
+                            <div className="gradient"/>
+                        </div>
                         </SplideSlide>
                     )
                 })}
                 </Splide>
-            </Wrapper>
+            </div>
             
-    </>
+    </div>
   )
 }
 
-const Wrapper = styled.div`
-margin: 4rem 0rem;
-`;
 
-const Card = styled.div
-`
-    min-height: 25rem;
-    border-radius: 2rem;
-    overflow: hidden;
-
-    img {
-        border-radius: 2rem;
-        position: absolute;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
-    p {
-        position: absolute;
-        z-index: 10;
-        left: 50%;
-        bottom: 0%;
-        transform: translate(-50%, 0%);
-        color: white;
-        width: 100%;
-        text-align: center;
-        font-weight: 600;
-        font-size: 1rem;
-        height: 40%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-`;
-
-const Gradient = styled.div`
-    z-index: 3;
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5))
-`
 export default Popular
