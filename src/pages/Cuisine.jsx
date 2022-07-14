401eeeb1f06177eb98df84bc3644b877baa15438
Jared Gutkin <jgutkinsam@gmail.com>
2022-07-14 14:05:35 -0500

    linked up cusines to api

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,7 +1,35 @@
+import {motion} from 'framer-motion'
+import { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
 
 function Cuisine() {
+
+    const [cuisine, setCuisine] = useState([])
+    let params = useParams()
+
+    const getCusine = async (name) => {
+        const data = await fetch(
+            `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`
+            );
+        const recipes = await data.json()
+        setCuisine(recipes.results)
+    }
+
+    useEffect(() => {
+        getCusine(params.type)
+        console.log(params.type)
+    }, [params.type])
   return (
-    <div>Cuisine</div>
+    <div className='grid'>
+        {cuisine.map((item) => {
+            return(
+                <div className="grid-card" key={item.id}>
+                    <img src={item.image} alt={item.title} />
+                    <h4>{item.title}</h4>
+                </div>
+            )
+        })}
+    </div>
   )
 }
 
