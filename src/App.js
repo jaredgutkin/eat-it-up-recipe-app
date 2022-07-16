4d9d6737227a17cd448948420af63ce887a87381
Jared Gutkin <jgutkinsam@gmail.com>
2022-07-16 09:42:23 -0500

    added header and home button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import Pages from "./pages/Pages";
 import Category from "./components/Category";
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter, Link} from 'react-router-dom'
 import Search from "./components/Search";
+import {GiKnifeFork} from 'react-icons/gi'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <div className="nav">
+        <GiKnifeFork/>
+        <Link to={'/'}>
+        <div className="logo"  >
+          Eat It Up
+        </div>
+        </Link>
+      </div>
       <Search/>
       <Category/>
       <Pages/>
